be3e0b81b43207bda61a5431c1882bd0506160bb
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-15 20:42:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName1 = "Scuber";
+const companyName =  "Scuber";
+// let mostProfitableNeighborhood;
+mostProfitableNeighborhood = "Chelsea";
+let  mostProfitableNeighborhood = "Chelsea";
+// let companyCeo;
+companyCeo = "Susan Smith";
+let companyCeo = "Susan Smith";
